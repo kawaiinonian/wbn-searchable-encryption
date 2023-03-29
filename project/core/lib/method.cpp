aadb51338c76005b618a6036180cbaafa86a28b6
secreu <1561750471@qq.com>
2023-03-29 22:56:39 +0800

    文件完全加解密实现

--- project/core/lib/method.cpp
+++ project/core/lib/method.cpp
@@ -1,8 +1,8 @@
 #include "method.h"
 
-char *read_file(const char *path, int &len) {
+uint8_t *read_file(const char *path, size_t &len) {
 	FILE *fp;
-	char *data;
+	uint8_t *data;
  
 	fp = fopen(path, "rb");
 	if (fp == NULL) {
@@ -11,7 +11,7 @@ char *read_file(const char *path, int &len) {
 
 	fseek(fp, 0, SEEK_END);
 	len = ftell(fp);
-	data = (char *)malloc((len + 1) * sizeof(char));
+	data = (uint8_t *)malloc((len + 1) * sizeof(uint8_t));
 	rewind(fp);
 	len = fread(data, 1, len, fp);
 	data[len] = '\0';
@@ -20,7 +20,7 @@ char *read_file(const char *path, int &len) {
     return data;
 }
 
-int write_file(const char *path, char *data, int len) {
+int write_file(const char *path, uint8_t *data, size_t len) {
     FILE * fp = fopen(path, "wb");
     if (fp) {
         assert(len = fwrite(data, 1, len, fp));
@@ -33,24 +33,71 @@ int write_file(const char *path, char *data, int len) {
     return 0;
 }
 
-int f_aes_cbc_enc(const char *path, uint8_t *key, int key_len, uint8_t *iv) {
+int f_aes_cbc_enc(const char *in_path, const char *out_path, const uint8_t *key, size_t key_len, const uint8_t *iv) {
+    FILE *fp;
+    uint8_t *plaintxt, *ciphertxt;
+    size_t in_len, out_len;
+
+    plaintxt = (uint8_t *)read_file(in_path, in_len);
+    if (plaintxt == NULL) {
+        return 1;
+    }
+
+    if (bc_aes_cbc_enc(ciphertxt, &out_len, plaintxt, in_len, key, key_len, iv)) {
+        // uint8_t *p1, *p2;
+        // for (p1 = p2 = (uint8_t *)path; p1 != '\0'; p1++) {
+        //     if (*p1 == '/') {
+        //         p2 = p1;
+        //     }
+        // }
+        if (!write_file(out_path, ciphertxt, out_len)) {
+            return 1;
+        }
+    }
 
     return 0;
 }
 
-int f_aes_cbc_dec(const char *path, uint8_t *key, int key_len, uint8_t *iv) {
+int f_aes_cbc_dec(const char *in_path, const char *out_path, const uint8_t *key, size_t key_len, const uint8_t *iv) {
+    FILE *fp;
+    uint8_t *ciphertxt, *plaintxt;
+    size_t in_len, out_len;
+
+    ciphertxt = (uint8_t *)read_file(in_path, in_len);
+    if (ciphertxt == NULL) {
+        return 1;
+    }
+
+    if (bc_aes_cbc_dec(plaintxt, &out_len, ciphertxt, in_len, key, key_len, iv)) {
+        if (!write_file(out_path, plaintxt, out_len)) {
+            return 1;
+        }
+    }
 
     return 0;
 }
 
 int main() {
-    int len;
-    char *data = read_file("1.txt", len);
+    const char *path = "1.txt";
+    const char *enc_path = "enc.txt";
+    const char *dec_path = "dec.txt";
+    uint8_t *seed = (uint8_t *)"1234567812345678123456781234567812345678123456781234567812345678";
+    size_t key_len = 64;
+    uint8_t *key = (uint8_t *)malloc((key_len + 1) * sizeof(uint8_t));
+    uint8_t *iv = (uint8_t *)malloc((key_len + 1) * sizeof(uint8_t));
 
-    cout << data << endl;
-    cout << len << endl;
+    rand_init();
+    rand_seed(seed, RLC_RAND_SEED);
+    rand_bytes(key, key_len);
+    rand_bytes(iv, key_len);
 
-    write_file("2.txt", data, len);
+    if (f_aes_cbc_enc(path, enc_path, key, key_len, iv)) {
+        cout << "Encryption Success!" << endl;
+    }
 
+    if (f_aes_cbc_dec(enc_path, dec_path, key, key_len, iv)) {
+        cout << "Decryption Success!" << endl;
+    }
+    
     return 0;
 }
--- project/core/lib/method.h
+++ project/core/lib/method.h
@@ -6,6 +6,7 @@
 
 #include <relic/relic.h>
 #include <relic/relic_bc.h>
+#include <relic/relic_rand.h>
 
 #include "../include/const.h"
 
@@ -17,7 +18,7 @@ using namespace std;
  * @param len 输出：文件长度
  * @return 文件内容
 ****************************************/
-char *read_file(const char *path, int &len);
+uint8_t *read_file(const char *path, size_t &len);
 
 /***************************************
  * @brief 写入文件
@@ -26,24 +27,25 @@ char *read_file(const char *path, int &len);
  * @param len 输入：文件长度
  * @return 0：成功 others：失败
 ****************************************/
-int write_file(const char *path, char *data, int len);
+int write_file(const char *path, uint8_t *data, size_t len);
 
 /***************************************
  * @brief 加密文件，结果会存放在同目录下
- * @param path 输入：文件路径
+ * @param in_path 输入：读文件路径
+ * @param out_path 输入：写文件路径
  * @param key 输入：密钥
  * @param key_len 输入：密钥长度
  * @param iv 输入：向量
  * @return 0：成功 others：失败
 ****************************************/
-int f_aes_cbc_enc(const char *path, uint8_t *key, int key_len, uint8_t *iv);
-
+int f_aes_cbc_enc(const char *in_path, const char *out_path, const uint8_t *key, size_t key_len, const uint8_t *iv);
 /***************************************
  * @brief 解密文件，结果会存放在同目录下
- * @param path 输入：文件路径
+ * @param in_path 输入：读文件路径
+ * @param out_path 输入：写文件路径
  * @param key 输入：密钥
  * @param key_len 输入：密钥长度
  * @param iv 输入：向量
  * @return 0: 成功 others: 失败
 ****************************************/
-int f_aes_cbc_dec(const char *path, uint8_t *key, int key_len, uint8_t *iv);
+int f_aes_cbc_dec(const char *in_path, const char *out_path, const uint8_t *key, size_t key_len, const uint8_t *iv);
