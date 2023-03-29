c6c700979834983d709da43b8efc7d9ce3d8e0f0
secreu <1561750471@qq.com>
2023-03-29 16:56:00 +0800

    文件完全读取写入函数原型和实现、文件加解密函数原型

--- project/core/lib/method.cpp
+++ project/core/lib/method.cpp
@@ -1,2 +1,57 @@
 #include "method.h"
 
+char *read_file(const char *path, int &len) {
+	FILE *fp;
+	char *data;
+ 
+	fp = fopen(path, "rb");
+	if (fp == NULL) {
+		return NULL;
+	}
+
+	fseek(fp, 0, SEEK_END);
+	len = ftell(fp);
+	data = (char *)malloc((len + 1) * sizeof(char));
+	rewind(fp);
+	len = fread(data, 1, len, fp);
+	data[len] = '\0';
+	fclose(fp);
+
+    return data;
+}
+
+int write_file(const char *path, char *data, int len) {
+    FILE * fp = fopen(path, "wb");
+    if (fp) {
+        assert(len = fwrite(data, 1, len, fp));
+        fclose(fp);
+        return 0;
+    } else {
+        return 1;
+    }
+    
+
+    return 0;
+}
+
+int f_aes_cbc_enc(const char *path, uint8_t *key, int key_len, uint8_t *iv) {
+
+    return 0;
+}
+
+int f_aes_cbc_dec(const char *path, uint8_t *key, int key_len, uint8_t *iv) {
+
+    return 0;
+}
+
+int main() {
+    int len;
+    char *data = read_file("1.txt", len);
+
+    cout << data << endl;
+    cout << len << endl;
+
+    write_file("2.txt", data, len);
+
+    return 0;
+}
--- project/core/lib/method.h
+++ project/core/lib/method.h
@@ -1,6 +1,49 @@
-#include <cryptlib.h>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cassert>
+#include <iostream>
+
+#include <relic/relic.h>
+#include <relic/relic_bc.h>
 
 #include "../include/const.h"
 
-using namespace CryptoPP;
+using namespace std;
 
+/***************************************
+ * @brief 读取文件
+ * @param path 输入：文件路径
+ * @param len 输出：文件长度
+ * @return 文件内容
+****************************************/
+char *read_file(const char *path, int &len);
+
+/***************************************
+ * @brief 写入文件
+ * @param path 输入：文件路径
+ * @param data 输入：文件内容
+ * @param len 输入：文件长度
+ * @return 0：成功 others：失败
+****************************************/
+int write_file(const char *path, char *data, int len);
+
+/***************************************
+ * @brief 加密文件，结果会存放在同目录下
+ * @param path 输入：文件路径
+ * @param key 输入：密钥
+ * @param key_len 输入：密钥长度
+ * @param iv 输入：向量
+ * @return 0：成功 others：失败
+****************************************/
+int f_aes_cbc_enc(const char *path, uint8_t *key, int key_len, uint8_t *iv);
+
+/***************************************
+ * @brief 解密文件，结果会存放在同目录下
+ * @param path 输入：文件路径
+ * @param key 输入：密钥
+ * @param key_len 输入：密钥长度
+ * @param iv 输入：向量
+ * @return 0: 成功 others: 失败
+****************************************/
+int f_aes_cbc_dec(const char *path, uint8_t *key, int key_len, uint8_t *iv);
