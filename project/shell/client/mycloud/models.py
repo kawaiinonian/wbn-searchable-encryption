b0610ad50f2958ec8b45a3b3a3744a2c0138b141
secreu <1561750471@qq.com>
2023-05-15 15:58:52 +0800

    数据库模型完善

--- project/shell/client/mycloud/models.py
+++ project/shell/client/mycloud/models.py
@@ -2,18 +2,37 @@ from django.db import models
 
 # Create your models here.
 
-class USERS(models.Model):
-    userid = models.AutoField(primary_key=True)
+class USER(models.Model):
     username = models.CharField(max_length=45)
     password = models.CharField(max_length=45)
 
-class SKEYS(models.Model):
-    userid = models.OneToOneField(to=USERS, to_field='userid', on_delete=models.CASCADE, primary_key=True)
+class SKEY(models.Model):
+    id = models.OneToOneField(to=USER, to_field='id', on_delete=models.CASCADE, primary_key=True)
     sk1 = models.CharField(max_length=45)
     sk2 = models.CharField(max_length=45)
     sk3 = models.CharField(max_length=45)
 
-class UKEYS(models.Model):
-    userid = models.OneToOneField(to=USERS, to_field='userid', on_delete=models.CASCADE, primary_key=True)
+class UKEY(models.Model):
+    id = models.OneToOneField(to=USER, to_field='id', on_delete=models.CASCADE, primary_key=True)
     uk1 = models.CharField(max_length=45)
     uk2 = models.CharField(max_length=45)
+
+class USRAUTH(models.Model):
+    id = models.ForeignKey(to=USER, to_field='id', on_delete=models.CASCADE)
+    d = models.CharField(max_length=45)
+    uid = models.CharField(max_length=45)
+    offtok = models.CharField(max_length=45)
+    class Meta:
+        unique_together = (("id", "d"),)
+
+class DOCKEY(models.Model):
+    id = models.ForeignKey(to=USER, to_field='id', on_delete=models.CASCADE)
+    d = models.CharField(max_length=45)
+    kd = models.CharField(max_length=45)
+    kdenc = models.CharField(max_length=45)
+    class Meta:
+        unique_together = (("id", "d"),)
+
+class AID(models.Model):
+    id = models.OneToOneField(to=USER, to_field='id', on_delete=models.CASCADE, primary_key=True)
+    aid = models.CharField(max_length=45)
