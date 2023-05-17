d6df14e005ec2872fc2139e66a00e2b83c0a1b26
secreu <1561750471@qq.com>
2023-05-17 20:00:23 +0800

    模型调整，使用内置User模型

--- project/shell/client/mycloud/models.py
+++ project/shell/client/mycloud/models.py
@@ -1,38 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class USER(models.Model):
-    username = models.CharField(max_length=45)
-    password = models.CharField(max_length=45)
+class SKey(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE, primary_key=True)
+    sk1 = models.BinaryField()
+    sk2 = models.BinaryField()
+    sk3 = models.BinaryField()
 
-class SKEY(models.Model):
-    id = models.OneToOneField(to=USER, to_field='id', on_delete=models.CASCADE, primary_key=True)
-    sk1 = models.CharField(max_length=45)
-    sk2 = models.CharField(max_length=45)
-    sk3 = models.CharField(max_length=45)
+class UKey(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE, primary_key=True)
+    uk1 = models.BinaryField()
+    uk2 = models.BinaryField()
 
-class UKEY(models.Model):
-    id = models.OneToOneField(to=USER, to_field='id', on_delete=models.CASCADE, primary_key=True)
-    uk1 = models.CharField(max_length=45)
-    uk2 = models.CharField(max_length=45)
-
-class USRAUTH(models.Model):
-    id = models.ForeignKey(to=USER, to_field='id', on_delete=models.CASCADE)
-    d = models.CharField(max_length=45)
-    uid = models.CharField(max_length=45)
-    offtok = models.CharField(max_length=45)
+class UsrAuth(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    d = models.BinaryField()
+    uid = models.BinaryField()
+    offtok = models.BinaryField()
     class Meta:
-        unique_together = (("id", "d"),)
+        unique_together = (("user", "d"),)
 
-class DOCKEY(models.Model):
-    id = models.ForeignKey(to=USER, to_field='id', on_delete=models.CASCADE)
-    d = models.CharField(max_length=45)
-    kd = models.CharField(max_length=45)
-    kdenc = models.CharField(max_length=45)
+class DocKey(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    d = models.BinaryField()
+    kd = models.BinaryField()
+    kdenc = models.BinaryField()
     class Meta:
-        unique_together = (("id", "d"),)
+        unique_together = (("user", "d"),)
 
-class AID(models.Model):
-    id = models.OneToOneField(to=USER, to_field='id', on_delete=models.CASCADE, primary_key=True)
-    aid = models.CharField(max_length=45)
+class Aid(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE, primary_key=True)
+    aid = models.BinaryField()
